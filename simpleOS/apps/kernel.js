686fba2b331ccd6677e4bce67ea3371cd1aa37a1
loulou123546 <loulou.valleres@live.fr>
2017-01-22 10:51:38 +0100

    modif on library

--- simpleOS/apps/kernel.js
+++ simpleOS/apps/kernel.js
@@ -2,6 +2,13 @@ let app_kernel = {
     start : function () {
         lib_console.clear();
         lib_console.write("Simple Kernel online V 0.0.1<br>login : ");
-        lib_console.read();
+        //login = lib_console.read();
+        lib_console.onread = true
+        /*lib_console.write("<br>password : ");
+        password = lib_console.read();
+        if(login == "louis" && password == "loulou123"){
+            lib_console.write("<br>Connection etablished !");
+        }*/
+        console.log("script finish");
     }
 }
--- simpleOS/library/console.js
+++ simpleOS/library/console.js
@@ -6,6 +6,43 @@ let lib_console = {
         lib_machine.setscreen(lib_machine.getscreen() + text);
     },
     read : function () {
-        lib_machine.setscreen(lib_machine.getscreen() + "<br><input type='text' style='display:inline;background-color: black;color: white;' autofocus>");
-    }
+        lib_console.startread();
+        while(lib_console.checkreadstate() == false){
+            console.log("on reading ...");
+            lib_console.waitfor(500);
+        }
+        lib_machine.setscreen(lib_machine.getscreen() + lib_console.textreaded);
+        return lib_console.textreaded;
+    },
+    startread : function () {
+        console.log("startread actived");
+        lib_console.textreaded = "";
+        lib_console.onread = true;
+    },
+    endread : function (event) {
+        console.log("endread actived with onread = "+lib_console.onread);
+        if(lib_console.onread == true){
+            if(event.keyCode == 13){
+                lib_console.onread = false;
+            } else {
+                lib_console.textreaded += String.fromCharCode(event.keyCode || event.charCode)
+                lib_console.write(String.fromCharCode(event.keyCode || event.charCode));
+                console.log(String.fromCharCode(event.keyCode || event.charCode));
+            }
+        }
+    },
+    checkreadstate : function () {
+        console.log("checked onread state with onread = "+lib_console.onread);
+        if(lib_console.onread == true){
+            return false;
+        } else {
+            return true;
+        }
+    },
+    waitfor : function (ms) {
+        ms += new Date().getTime();
+        while (new Date() < ms){}
+    },
+    textreaded : "",
+    onread : false
 }
